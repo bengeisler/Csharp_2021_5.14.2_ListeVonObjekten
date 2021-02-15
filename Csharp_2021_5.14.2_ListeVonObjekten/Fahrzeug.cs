49e8adf4a3634ce7b95a59746b117c4439b2285b
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-15 14:25:29 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_5.14.2_ListeVonObjekten/Fahrzeug.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_2021_5._14._2_ListeVonObjekten
+{
+	class Fahrzeug : IEquatable<Fahrzeug>, IComparable<Fahrzeug>
+	{
+		// Eigenschaften
+		private int geschwindigkeit;
+		// readonly: Eigenschaft kann nur im Konstruktor geschrieben werden
+		private readonly string bezeichnung;
+
+		// Konstruktor
+		public Fahrzeug(int g, string b)
+		{
+			geschwindigkeit = g;
+			bezeichnung = b;
+		}
+
+		// Methoden
+		public void Beschleunigen(int wert) => geschwindigkeit += wert;
+
+		public override string ToString()
+		{
+			return "Bezeichnung: " + bezeichnung + "\n" +
+				"Geschwindigkeit: " + geschwindigkeit;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Fahrzeug);
+		}
+
+		public bool Equals(Fahrzeug other)
+		{
+			return other != null &&
+						 geschwindigkeit == other.geschwindigkeit &&
+						 bezeichnung == other.bezeichnung;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = -990109093;
+			hashCode = hashCode * -1521134295 + geschwindigkeit.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(bezeichnung);
+			return hashCode;
+		}
+
+		// Vergleichsfuntion
+		// Gibt an, ob das Objekt "other":
+		// - vor (-1) 
+		// - an der gleichen Stelle (0)
+		// - oder hinter (1) dem aktuellen Objekt steht
+
+		// => Muss implementiert werden, damit Sort() funktioniert
+		public int CompareTo(Fahrzeug other)
+		{
+			return geschwindigkeit.CompareTo(other.geschwindigkeit);
+		}
+	}
+}
---
+++ Csharp_2021_5.14.2_ListeVonObjekten/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_2021_5._14._2_ListeVonObjekten
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// Liste zur Aufnahme der Fahrzeuge anlegen
+			// List<Fahrzeug> meineListe = new List<Fahrzeug>();	
+			var meineListe = new List<Fahrzeug>();
+
+			// Fahrzeug-Objekte erstellen
+			Fahrzeug bmw = new Fahrzeug(100, "BMW");
+			var polo = new Fahrzeug(80, "Polo");
+			var golf = new Fahrzeug(130, "Golf");
+			var honda = new Fahrzeug(120, "Honda");
+
+			// Fahrzeuge der Liste hinzufügen
+			meineListe.Add(bmw);
+			meineListe.Add(polo);
+			meineListe.Add(golf);
+			meineListe.Add(honda);
+
+			// Elemente der Liste ausgeben
+			Console.WriteLine("Die Elemente der Liste:");
+			foreach (var fahrzeug in meineListe)
+			{
+				Console.WriteLine(fahrzeug);
+			}
+
+			// Element in der Liste suchen
+			Console.WriteLine("Das vorhandene Objekt bmw wird gesucht:");
+			Console.WriteLine(meineListe.Contains(bmw));
+
+			// Damit die Eigenschaften verglichen werden, muss die Klasse
+			// IEquatable<T> implementieren
+			var gesuchtesFahrzeug = new Fahrzeug(100, "BMW");
+			Console.WriteLine("Das neu erzeugte Fahrzeug BMW wird gesucht: ");
+			Console.WriteLine(meineListe.Contains(gesuchtesFahrzeug));
+
+			// Liste sortieren
+			// => Klasse muss IComparable<T> implementieren
+			Console.WriteLine("Sortierte Liste:");
+			meineListe.Sort();
+			foreach (var fahrzeug in meineListe)
+			{
+				Console.WriteLine(fahrzeug);
+			}
+		}
+	}
+}
